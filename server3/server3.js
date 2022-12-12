f0b47f0b064b7029c4b1eef613055b0928081bc6
Oleg <dubrovin.oleg.v@mail.ru>
2022-12-12 14:31:24 +0300

    123

--- server3/server3.js
+++ server3/server3.js
@@ -3,10 +3,10 @@
 // const net = require('net');
 
 const io = require('socket.io-client');
-const socket1 = io.connect("http://85.193.88.165:3001/", {
+const socket1 = io.connect("http://85.193.88.165:3001", {
     reconnection: true
 });
-const socket2 = io.connect("http://85.193.88.165:3002/", {
+const socket2 = io.connect("http://85.193.88.165:3002", {
     reconnection: true
 });
 
@@ -24,6 +24,7 @@ const writeFile = (content) => {
 
 socket1.on('connect', function () {
 
+    console.log(`connected to server-${1}`)
     writeFile(`connected to server-${1}`)
     
     socket1.emit("join_room", server_room)
@@ -31,25 +32,30 @@ socket1.on('connect', function () {
     socket1.on('log_request', data => {
         const time = new Date().toString()
         writeFile(`Server-${1}: [time: ${time}, data: ${data}]`)
+        console.log(`Server-${1}: [time: ${time}, data: ${data}]`)
     });
 });
 
 socket2.on('connect', function () {
     writeFile(`connected to server-${2}`);
+    console.log(`connected to server-${2}`)
 
     socket2.emit("join_room", server_room)
 
     socket2.on('log_request', data => {
         const time = new Date().toString()
         writeFile(`Server-${2}: [time: ${time}, data: ${data}]`)
+        console.log(`Server-${2}: [time: ${time}, data: ${data}]`)
     });
 });
 
 
 socket1.on("disconnect", () => {
     writeFile(`Server-${1} Disconnected`, socket1.id);
+    console.log(`Server-${1} Disconnected`, socket1.id)
 });
 
 socket2.on("disconnect", () => {
     writeFile(`Server-${2} Disconnected`, socket2.id);
+    console.log(`Server-${2} Disconnected`, socket2.id)
 });
